544c658d4a2fea5edaf5eaf7d9533a35080e5013
JManuel Ruiz <metalfranckie@gmail.com>
2013-11-12 21:52:50 -0600

    Generador de elementos para backbone como AMD

--- tasks/bb_pep_generate.js
+++ tasks/bb_pep_generate.js
@@ -8,43 +8,92 @@
 
 'use strict';
 
+
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+  grunt.registerMultiTask('bb_pep_generate', 'The best Grunt plugin ever.', function( name ) {
 
-  grunt.registerMultiTask('bb_pep_generate', 'The best Grunt plugin ever.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+    var options = this.options({ source: 'app/js/backbone' });
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+    var elements = (function() {
+      var elements = [
+        { tplname: 'view.js'       ,extension: '.js'   ,name: 'view'       },
+        { tplname: 'model.js'      ,extension: '.js'   ,name: 'model'      },
+        { tplname: 'collection.js' ,extension: '.js'   ,name: 'collection' },
+        { tplname: 'router.js'     ,extension: '.js'   ,name: 'router'     },
+        { tplname: 'module.js'     ,extension: '.js'   ,name: 'module'     },
+        { tplname: 'template.js'   ,extension: '.html' ,name: 'template'   }
+      ];
+
+      var _get = function(name) {
+        if (typeof name === 'undefined' || !name)
+        {
+          return null;
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
 
-      // Handle options.
-      src += options.punctuation;
+        for (var i = 0; i < elements.length; i++) {
+          if( elements[i].name === name )
+          {
+            return elements[i];
+          }
+        }
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+        return null;
+      };
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
+      return {
+        has: function(name) {
+          return (_get(name)!==null)? true : false;
+        },
+        get: function(name) {
+          return _get(name);
+        }
+      };
+    })();
+
+    var ElementCreator = function (el, options) {
+      var template, tplpath;
+
+      tplpath  = __dirname + '/../templates/' + el.tplname;
+      template = grunt.file.read( tplpath );
+
+      return {
+        make: function( name ) {
+          if( typeof name === 'undefined' ){
+            grunt.fail.warn('the name has not been specified.');
+            // return grunt.log.error('Missing name.');
+            name = 'name';
+          }
+          // BUG: Incluir pluralizacion
+          var filepath = options.source +'/'+ el.name +'s/'+ name + el.extension;
+
+          if( grunt.file.exists(filepath) ){
+            grunt.fail.warn('File already exists ('+ filepath +').');
+          }
+
+          var content, message, data;
+
+          data = {
+            name: name,
+            options: options
+          };
+ 
+          content = grunt.template.process(template, { data:data });
+          grunt.file.write(filepath, content);
+
+          message = 'Great! '+ name +' '+ el.name +' has been created ('+ filepath +').';
+          grunt.log.writeln(message);
+
+        }
+      };
+    };
+
+    if (!elements.has(this.target)) {
+      grunt.fail.fatal('Missing arguments');
+    }
+
+    var f = new ElementCreator(elements.get(this.target), options);
+    f.make( name );
   });
 
 };
